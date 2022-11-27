d5fd376efa888dfdfbeb60acf3350f75fcf1f316
Mikhail <larionov404.mu@gmail.com>
2022-11-27 21:02:36 +0300

    commit

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ window.addEventListener('load', function() {
   let allowedExtensions = /(.png|.jpg|.jpeg)$/i; // @: валидация
   let data = new FormData()
 
+  // ?: ФУНКЦИЯ ПЕРЕКУЛЮЧЕНИЯ ПРЕЛООДЕРА
+  function changePreloader (bol) {
+    let preloader = document.querySelector('.photo-preloader-ring');
+    if (bol == true) {
+      preloader.classList.remove('hidden');
+    } else {
+      preloader.classList.add('hidden');
+    }
+  }
+
   // ?: ВЫБОР ПРЕДЗАГРУЖАЕМОГО ШАГА
   function changeStep (step) {
     let stepAll = document.querySelectorAll('.wrapper_content .step-upload');
@@ -9,6 +19,8 @@ window.addEventListener('load', function() {
       let dataStep = el.getAttribute('data-step_id');
       if(dataStep == step) {
         el.classList.remove('hidden');
+      } else {
+        el.classList.add('hidden');
       }
     });
   }
@@ -33,161 +45,60 @@ window.addEventListener('load', function() {
   })
 
   // !: ПЕРЕМЕННЫЕ БЛОКОВ
-  let firstPassportFileInput = document.getElementById('first_passport_input');
-  let secondPassportFileInput = document.getElementById('second_passport_input');
+  let uploadPhoto = document.querySelectorAll('.upload-photo'); 
 
-  let firstCardFileInput = document.getElementById('first-card-input');
-  let secondCardFileInput = document.getElementById('second-card-input');
-  let thirdCardFileInput = document.getElementById('third-card-input');
+  // ?: ЗАГРУЗКА ФОТО
+  uploadPhoto.forEach(item => {
+    item.addEventListener('change', function() {
+      let main = item.closest('.step-upload');
+      let img = main.querySelector('.upload_file_img');
 
+      console.log(document.querySelector('.wrapper_content_passport_error_window_info__img').querySelector('img'))
 
-  // ?: ПЕРВАЯ ФОТОГРАФИЯ ПАСПОРТА
-  firstPassportFileInput.addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-      let filePath = firstPassportFileInput.value;
-      let img = document.getElementById('first_passport_img');
-      if (!allowedExtensions.exec(filePath)) {
-        alert('Недопустимый тип файла');
-        fileInput.value = '';
-        return false;
-      } else {
-        img.onload = () => URL.revokeObjectURL(img.src);
-        img.src = URL.createObjectURL(this.files[0]);
-        fetch('yourAPI', { // @: ваш POST
-          method: 'POST',
-          headers: {
-            "Content-Type": "First passport file"
-          },
-          body: img.src // @: наш файл
-        }).then (
-          response => response.json() // @: если ответ является объектом JSON
-        ).then (
-          success => console.log(success) // @: обработка объекта ответа на успешный процесс
-        ).catch (
-          error => console.log(error) // @: обработка объекта ответа на ошибку
-        );
-        document.getElementById('passport-step-one').classList.add('hidden');
-        document.getElementById('passport-step-two').classList.remove('hidden');
-      }
-    }
+      changePreloader(true);
+      setTimeout(() => {
+        if (this.files && this.files[0]) {
+          let filePath = item.value;
+          if (!allowedExtensions.exec(filePath)) {
+            alert('Недопустимый тип файла');
+            fileInput.value = '';
+            return false;
+          } else {
+            img.onload = () => URL.revokeObjectURL(img.src);
+            img.src = URL.createObjectURL(this.files[0]);
+            document.querySelector('.wrapper_content_passport_error_window_info__img').querySelector('img').src = img.src; // @: загрузка изображения в окно ошибки
+            document.querySelector('.wrapper_content_passport_error_window_info__img-fullscrean').querySelector('img').src = img.src; // @: загрузка изображения в полноразмерное окно ошибки
+            fetch('', { // @: ваш POST
+              method: 'POST',
+              headers: {
+                "Content-Type": "First passport file"
+              },
+              body: img.src // @: наш файл
+            }).then (
+              response => response.json() // @: если ответ является объектом JSON
+            ).then (
+              success => console.log(success) // @: обработка объекта ответа на успешный процесс
+            ).catch (
+              error => console.log(error) // @: обработка объекта ответа на ошибку
+            );
+
+            let stepId = item.closest('.step-upload').getAttribute('data-step_id');
+            changeStep(parseInt(stepId) + 1);
+          };
+        };
+        changePreloader(false);
+        document.querySelector('.wrapper_content_passport_error_window').classList.remove('hidden')
+      }, "3000")
+    });
   });
-
-  // ?: ВТОРАЯ ФОТОГРАФИЯ ПАСПОРТА
-  secondPassportFileInput.addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-      let filePath = secondPassportFileInput.value;
-      let img = document.getElementById('second_passport_img');
-      if (!allowedExtensions.exec(filePath)) {
-        alert('Недопустимый тип файла');
-        fileInput.value = '';
-        return false;
-      } else {
-        img.onload = () => URL.revokeObjectURL(img.src);
-        img.src = URL.createObjectURL(this.files[0]);
-        fetch('yourAPI', { // @: ваш POST
-          method: 'POST',
-          headers: {
-            "Content-Type": "First passport file"
-          },
-          body: img.src // @: наш файл
-        }).then (
-          response => response.json() // @: если ответ является объектом JSON
-        ).then (
-          success => console.log(success) // @: обработка объекта ответа на успешный процесс
-        ).catch (
-          error => console.log(error) // @: обработка объекта ответа на ошибку
-        );
-        document.getElementById('passport-step-two').classList.add('hidden');
-        document.getElementById('first-card-front').classList.remove('hidden');
-      }
-    }
-  });
-
-  // ?: ПЕРВАЯ ФОТОГРАФИЯ КАРТЫ
-  firstCardFileInput.addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-      let filePath = firstCardFileInput.value;
-      let img = document.getElementById('first-card-img');
-      if (!allowedExtensions.exec(filePath)) {
-        alert('Недопустимый тип файла');
-        fileInput.value = '';
-        return false;
-      } else {
-        img.onload = () => URL.revokeObjectURL(img.src);
-        img.src = URL.createObjectURL(this.files[0]);
-        fetch('yourAPI', { // @: ваш POST
-          method: 'POST',
-          headers: {
-            "Content-Type": "First passport file"
-          },
-          body: img.src // @: наш файл
-        }).then (
-          response => response.json() // @: если ответ является объектом JSON
-        ).then (
-          success => console.log(success) // @: обработка объекта ответа на успешный процесс
-        ).catch (
-          error => console.log(error) // @: обработка объекта ответа на ошибку
-        );
-      }
-    }
-  });
-
-  // ?: ВТОРАЯ ФОТОГРАФИЯ КАРТЫ
-  secondCardFileInput.addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-      let filePath = secondCardFileInput.value;
-      let img = document.getElementById('second-card-img');
-      if (!allowedExtensions.exec(filePath)) {
-        alert('Недопустимый тип файла');
-        fileInput.value = '';
-        return false;
-      } else {
-        img.onload = () => URL.revokeObjectURL(img.src);
-        img.src = URL.createObjectURL(this.files[0]);
-        fetch('yourAPI', { // @: ваш POST
-          method: 'POST',
-          headers: {
-            "Content-Type": "First passport file"
-          },
-          body: img.src // @: наш файл
-        }).then (
-          response => response.json() // @: если ответ является объектом JSON
-        ).then (
-          success => console.log(success) // @: обработка объекта ответа на успешный процесс
-        ).catch (
-          error => console.log(error) // @: обработка объекта ответа на ошибку
-        );
-      }
-    }
-  });
-
-  // ?: ТРЕТЬЯ ФОТОГРАФИЯ КАРТЫ
-  thirdCardFileInput.addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-      let filePath = thirdCardFileInput.value;
-      let img = document.getElementById('third-card-img');
-      if (!allowedExtensions.exec(filePath)) {
-        alert('Недопустимый тип файла');
-        fileInput.value = '';
-        return false;
-      } else {
-        img.onload = () => URL.revokeObjectURL(img.src);
-        img.src = URL.createObjectURL(this.files[0]);
-        fetch('yourAPI', { // @: ваш POST
-          method: 'POST',
-          headers: {
-            "Content-Type": "First passport file"
-          },
-          body: img.src // @: наш файл
-        }).then (
-          response => response.json() // @: если ответ является объектом JSON
-        ).then (
-          success => console.log(success) // @: обработка объекта ответа на успешный процесс
-        ).catch (
-          error => console.log(error) // @: обработка объекта ответа на ошибку
-        );
-      }
-    }
-  });
-
 });
+
+document.querySelector('.wrapper_content_passport_error_window__btn').addEventListener('click', () => { document.querySelector('.wrapper_content_passport_error_window').classList.add('hidden') })
+
+// ?: ИЗОБРАЖЕНИЕ НА ВЕСЬ ЭКРАН
+document.querySelector('.wrapper_content_passport_error_window_info__img').addEventListener('click', () => {
+  document.querySelector('.wrapper_content_passport_error_window_info__img-fullscrean').classList.toggle('hidden');
+})
+document.querySelector('.wrapper_content_passport_error_window_info__img-fullscrean').addEventListener('click', () => {
+  document.querySelector('.wrapper_content_passport_error_window_info__img-fullscrean').classList.toggle('hidden');
+})
